d7c7344e5c71ef6e361b9d33c400a308805dcdcd
Marcel <marceldjnairo@gmail.com>
2024-10-07 21:22:23 +0200

    Update config

--- config/workflows.php
+++ config/workflows.php
@@ -1,16 +1,13 @@
 <?php
 
 return [
-    'register' => [
-        'models' => [
-            'workflow' => \Tschucki\FilamentWorkflows\Models\WorkflowLog::class,
-            'workflow_log' => \Tschucki\FilamentWorkflows\Models\WorkflowLog::class,
-        ],
-        'resources' => [
-            'workflow' => \Tschucki\FilamentWorkflows\Resources\FilamentWorkflowResource::class,
-        ],
+    'models' => [
+        'workflow' => \Tschucki\FilamentWorkflows\Models\WorkflowLog::class,
+        'workflow_log' => \Tschucki\FilamentWorkflows\Models\WorkflowLog::class,
     ],
-
+    'resources' => [
+        'workflow' => \Tschucki\FilamentWorkflows\Resources\FilamentWorkflowResource::class,
+    ],
     'search' => [
         'max_results' => 100,
     ],
